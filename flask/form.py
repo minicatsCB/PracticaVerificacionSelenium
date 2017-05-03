fb386829e79a284f4434a576000d6e32b91a4188
minicatsCB <maribeldiaz96@yahoo.es>
2017-05-04 00:11:23 +0200

    Add basic form application

---
+++ flask/form.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+	return render_template('form.html')
+
+@app.route('/signup', methods = ['POST'])
+def signup():
+	if request.method == 'POST':
+		result = request.form
+		return render_template('result.html', result = result)
+
+
+if __name__ == "__main__":
+	app.run(debug = True)
+
